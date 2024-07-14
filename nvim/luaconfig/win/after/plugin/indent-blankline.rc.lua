96f8bdd6162e3c12caf65e1f83d670f586a778cc
LINK <mucylulu@163.com>
2024-07-14 16:33:14 +0800

    nvim lua配置更新

---
+++ nvim/luaconfig/win/after/plugin/indent-blankline.rc.lua
@@ -0,0 +1,27 @@
+-- 本来是与 Rainbow-Delimeters.nvim 配合使用,但是这个插件会造成一点卡顿，所以简化了配置
+local highlight = {
+  -- "RainbowRed",
+  -- "RainbowYellow",
+  "RainbowBlue",
+  -- "RainbowOrange",
+  -- "RainbowGreen",
+  -- "RainbowViolet",
+  -- "RainbowCyan",
+}
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  -- vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+  -- vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+   vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+  -- vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+  -- vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+  -- vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+  -- vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+-- vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup { scope = { highlight = highlight } }
+
+-- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
---
+++ nvim/luaconfig/win/lua/config/keybinding.lua
@@ -0,0 +1,9 @@
+local set = vim.opt
+local cmd = vim.cmd
+local o = vim.o
+local g = vim.g
+local bo = vim.bo
+local map = vim.api.nvim_set_keymap 
+
+g.mapleader = ","
+g.maplocalleader = ","
