239f081eabb9a5a1eb1e96e8b4ef88c953d62453
LINKLIU <mucylulu@163.com>
2024-07-16 18:26:54 +0800

    update nvim config in windows platform

--- nvim/luaconfig/win/lua/plugins/plugin-telescope.nvim.lua
+++ nvim/luaconfig/win/lua/plugins/plugin-telescope.nvim.lua
@@ -2,5 +2,15 @@
 return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        require("telescope").setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    "%.meta",
+                }
+            }
+        })
+    end
 }
