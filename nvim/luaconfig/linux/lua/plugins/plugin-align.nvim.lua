02d36f20781128a2b0552277313d0fbbf7be2884
LINKLIU <mucylulu@163.com>
2024-07-31 18:37:29 +0800

    update config

--- nvim/luaconfig/linux/lua/plugins/plugin-align.nvim.lua
+++
@@ -1,9 +0,0 @@
---对齐插件
-return {
-    'Vonr/align.nvim',
-    branch = "v2",
-    lazy = true,
-    init = function ()
-        
-    end
-}
--- nvim/luaconfig/linux/lua/plugins/plugin-mini.nvim.lua
+++ nvim/luaconfig/linux/lua/plugins/plugin-mini.nvim.lua
@@ -1,3 +1,10 @@
 return {
-    'echasnovski/mini.nvim', version = false
+    'echasnovski/mini.nvim',
+    version = false,
+    config = function ()
+        require("mini.align").setup()
+        require("mini.icons").setup()
+        require("mini.notify").setup()
+    end
+
 }
