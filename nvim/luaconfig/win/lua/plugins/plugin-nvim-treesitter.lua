b81023fa9b02062d06afe1dd35052eed6a9d1cc0
LINK <mucylulu@163.com>
2024-08-11 11:07:55 +0800

    update config

--- nvim/luaconfig/win/lua/plugins/plugin-nvim-treesitter.lua
+++ nvim/luaconfig/win/lua/plugins/plugin-nvim-treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require 'nvim-treesitter.configs'.setup {
             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "markdown", "markdown_inline" },
+            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "regex", "bash"},
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
