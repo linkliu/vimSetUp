53351cdf6e7885c2e8c8b7d32c875bab6ed0d932
LINKLIU <mucylulu@163.com>
2026-04-15 11:29:08 +0800

    配置修改

--- nvim/luaconfig/win/lua/plugins/plugin-nvim-treesitter.lua
+++ nvim/luaconfig/win/lua/plugins/plugin-nvim-treesitter.lua
@@ -2,7 +2,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     config = function()
-        require 'nvim-treesitter.configs'.setup {
+        require 'nvim-treesitter'.setup {
             -- A list of parser names, or "all" (the listed parsers MUST always be installed)
             ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc", "markdown", "markdown_inline", "regex", "bash"},
 
--- nvim/luaconfig/win/lua/plugins/plugin-telescope.nvim.lua
+++ nvim/luaconfig/win/lua/plugins/plugin-telescope.nvim.lua
@@ -1,8 +1,11 @@
 -- plugins/telescope 文件搜索
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
+
+    },
     config = function()
         require("telescope").setup({
             defaults = {
